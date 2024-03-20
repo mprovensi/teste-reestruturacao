4a644b1586318209b9bdeaed1853e395650e7cfd
mprovensi <mprovensi@gmail.com>
2024-03-20 09:36:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# teste-reestruturacao
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mprovensi/teste-reestruturacao)
---
+++ src/example/tree-nested-overview-example.ts
@@ -0,0 +1,56 @@
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {Component} from '@angular/core';
+import {MatTreeNestedDataSource, MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Tribunal Pleno',
+    children: [
+      {
+        name: 'Presidência',
+        children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
+      },
+      {
+        name: 'Vice-Presidência',
+        children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
+      },
+    ],
+  },
+];
+
+/**
+ * @title Tree with nested nodes
+ */
+@Component({
+  selector: 'tree-nested-overview-example',
+  templateUrl: 'tree-nested-overview-example.html',
+  styleUrl: 'tree-nested-overview-example.css',
+  standalone: true,
+  imports: [MatTreeModule, MatButtonModule, MatIconModule],
+})
+export class TreeNestedOverviewExample {
+  treeControl = new NestedTreeControl<FoodNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FoodNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FoodNode) => !!node.children && node.children.length > 0;
+}
+
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
